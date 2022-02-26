ea0faf38202c7fee7027094ae4a7475bdce6dc5d
happyface33556 <happyface33556@gmail.com>
2022-02-26 16:16:25 -0500

    Changed Student & Admin View Screens to Application Layout with nav bar drop menu on side detailing options for each respective user

--- src/main/java/com/example/demo/AdminView.java
+++ src/main/java/com/example/demo/AdminView.java
@@ -1,12 +1,26 @@
 package com.example.demo;
 
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.router.Route;
 
 @Route("admin")
-public class AdminView extends VerticalLayout {
+public class AdminView extends AppLayout {
     public AdminView() {
-        add(new H1("This will be the ADMIN Screen"));
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("Hello Administrator!");
+        title.getStyle()
+                .set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+
+        Tab pendingSubs = new Tab("Pending Submissions");
+        Tab allSubs = new Tab("All Submissions");
+
+        addToDrawer(pendingSubs, allSubs);
+        addToNavbar(toggle, title);
     }
 }
--- src/main/java/com/example/demo/StudentView.java
+++ src/main/java/com/example/demo/StudentView.java
@@ -1,12 +1,26 @@
 package com.example.demo;
 
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 
 @Route("student")
-public class StudentView extends VerticalLayout {
+public class StudentView extends AppLayout {
     public StudentView() {
-        add(new H1("This will be the STUDENT Screen"));
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("Hello Student!");
+        title.getStyle()
+                .set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+
+        Tab newSub = new Tab("New Submission");
+        Tab allSub = new Tab("All Submissions");
+        addToDrawer(newSub, allSub);
+        addToNavbar(toggle, title);
     }
 }
