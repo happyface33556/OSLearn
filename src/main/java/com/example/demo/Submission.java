18a80288b01a077c44a99b9c66a3af3150820400
Eric McGuirk <mcguirk.eric@gmail.com>
2022-04-11 22:40:28 -0400

    Create User Class

--- src/main/java/com/example/demo/Submission.java
+++ src/main/java/com/example/demo/Submission.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 @Entity
 @Repository
 public class Submission {
@@ -29,76 +33,5 @@ public class Submission {
         this.link = link;
     }
 
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public Boolean getApproval() {
-        return approval;
-    }
-
-    public void setApproval(Boolean approval) {
-        this.approval = approval;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public int getNumLikes() {
-        return numLikes;
-    }
-
-    public void setNumLikes(int numLikes) {
-        this.numLikes = numLikes;
-    }
-
-    public int getNumDislikes() {
-        return numDislikes;
-    }
-
-    public void setNumDislikes(int numDislikes) {
-        this.numDislikes = numDislikes;
-    }
-
-    public double getUserScore() {
-        return userScore;
-    }
-
-    public void setUserScore(double userScore) {
-        this.userScore = userScore;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
 }
 
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fname;
+    private String lname;
+
+    private Type type;
+
+    enum Type {
+        STUDENT,
+        TEACHER,
+        ADMINISTRATOR
+    }
+}
