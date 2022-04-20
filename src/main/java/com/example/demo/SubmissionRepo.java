896ace183ea88a0b4cf8c4a6fa745aa80e789012
Eric McGuirk <mcguirk.eric@gmail.com>
2022-04-19 22:23:51 -0400

    Add FindAllByStatus() method

--- src/main/java/com/example/demo/SubmissionRepo.java
+++ src/main/java/com/example/demo/SubmissionRepo.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface SubmissionRepo extends JpaRepository<Submission, Long> {
 
+    ArrayList<Submission> findAllByStatus(Submission.Status status);
 }
