c2a39566b655b524fefd2e37151cccdaf1757660
Stone <stoneblumauer96@gmail.com>
2022-04-23 14:11:35 -0400

    Added icons to tabs and buttons

---
+++ src/main/java/com/example/demo/views/admin/AddCourseView.java
@@ -0,0 +1,16 @@
+package com.example.demo.views.admin;
+
+import com.example.demo.layouts.AdminLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "add-course", layout = AdminLayout.class)
+public class AddCourseView extends VerticalLayout { //todo:
+
+
+
+}
---
+++ src/main/java/com/example/demo/views/admin/CourseView.java
@@ -0,0 +1,12 @@
+package com.example.demo.views.admin;
+
+import com.example.demo.Course;
+import com.example.demo.layouts.AdminLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import java.util.ArrayList;
+
+@Route(value = "courses", layout = AdminLayout.class)
+public class CourseView extends VerticalLayout { //todo:
+
+}
