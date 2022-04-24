f48c2a85051e56d3afc1bd53a9e34bc3e8bb0e3b
Stone <stoneblumauer96@gmail.com>
2022-04-24 14:37:38 -0400

    Admin CourseView and AddCourse

--- src/main/java/com/example/demo/views/admin/AddCourseView.java
+++ src/main/java/com/example/demo/views/admin/AddCourseView.java
@@ -1,16 +1,61 @@
 package com.example.demo.views.admin;
 
+import com.example.demo.Course;
+import com.example.demo.CourseRepo;
+import com.example.demo.Submission;
+import com.example.demo.SubmissionRepo;
 import com.example.demo.layouts.AdminLayout;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 
 @Route(value = "add-course", layout = AdminLayout.class)
 public class AddCourseView extends VerticalLayout { //todo:
 
+    @Autowired
+    private SubmissionRepo submissionRepo;
+    @Autowired
+    private CourseRepo courseRepo;
+    Binder<Course> binder = new Binder<>(Course.class);
 
+    TextField courseName = new TextField("Course Name");
+    Button submit = new Button("Submit");
+    VerticalLayout verticalLayout = new VerticalLayout();
+
+
+
+    public AddCourseView (Submission repo, CourseRepo courseRepo) {
+        binder.bind(courseName, Course::getCourseName, Course::setCourseName);
+
+        binder.bindInstanceFields(this);
+        binder.setBean(new Course());
+
+        add(new H1("Add Course"),
+                new HorizontalLayout(courseName),
+                new HorizontalLayout(submit));
+        setHeightFull();;
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        submit.addClickListener(event -> save());
+        submit.addClickShortcut(Key.ENTER);
+
+    }
+
+    private void save() {
+        Course course = binder.getBean();
+        courseRepo.save(course);
+        UI.getCurrent().navigate("admin-home");
+    }
 
 }
--- src/main/java/com/example/demo/views/admin/CourseView.java
+++ src/main/java/com/example/demo/views/admin/CourseView.java
@@ -2,14 +2,41 @@ package com.example.demo.views.admin;
 
 import com.example.demo.Course;
 import com.example.demo.CourseRepo;
+import com.example.demo.Submission;
 import com.example.demo.SubmissionRepo;
 import com.example.demo.layouts.AdminLayout;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import java.util.ArrayList;
 import com.vaadin.flow.component.grid.Grid;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route(value = "courses", layout = AdminLayout.class)
 public class CourseView extends VerticalLayout {
+
+    @Autowired
+    private SubmissionRepo repo;
+
+    public CourseView(SubmissionRepo repo) {
+        ArrayList<Submission> submissions = repo.findAllByStatus(Submission.Status.YES);
+        ArrayList<String> courseNames = new ArrayList<>();
+        for (int i = 0; i < submissions.size(); i++) {
+            if (!courseNames.contains(submissions.get(i).getCourseName())) {
+                courseNames.add(submissions.get(i).getCourseName());
+            }
+        }
+        for (int i = 0; i < courseNames.size(); i++) {
+            Grid<Submission> submissionGrid = new Grid<>(Submission.class);
+            submissionGrid.setColumns("courseName", "link", "comments");
+            submissionGrid.setItems(repo.findByCourseName(courseNames.get(i)));
+            add(new H1(courseNames.get(i)));
+            add(submissionGrid);
+        }
+        setHeightFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+    }
+
 }
