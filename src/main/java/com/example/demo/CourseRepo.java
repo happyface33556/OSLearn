3d305fbdf07f14f63efb02ff1a493d4acff3451b
Eric McGuirk <mcguirk.eric@gmail.com>
2022-04-24 17:59:04 -0400

    Added additional CRUD functions

--- src/main/java/com/example/demo/CourseRepo.java
+++ src/main/java/com/example/demo/CourseRepo.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface CourseRepo extends CrudRepository<Course, String> {
+    ArrayList<Course> findByCourseName(String name);
 }
--- src/main/java/com/example/demo/UserRepo.java
+++ src/main/java/com/example/demo/UserRepo.java
@@ -2,5 +2,9 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface UserRepo extends CrudRepository<User, Long> {
+    ArrayList<User> findByUsername(String name);
+    ArrayList<User> findByFname(String name);
 }
